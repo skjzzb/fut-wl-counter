ab931dc0a1e957b33086881b2ee08cb9fc2c12f0
berkay <berkay.ozdemir@msn.com>
2023-01-16 13:16:50 +0300

    finished product

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,48 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [wins, setWins] = useState(0);
-  const [loses, setLoses] = useState(0);
+  const [wins, setWins] = useState(localStorage.getItem('wins') || 0);
+  const [loses, setLoses] = useState(localStorage.getItem('loses') || 0);
 
   const addWin = () => {
-    setWins((wins) => wins + 1);
+    if (wins >= 20) return;
+    setWins((wins) => parseInt(wins) + 1);
+    localStorage.setItem('wins', wins + 1);
   };
   const removeWin = (e) => {
     e.preventDefault();
-    setWins((wins) => wins - 1);
+    if (wins <= 0) return;
+    setWins((wins) => parseInt(wins) - 1);
+    localStorage.setItem('wins', wins - 1);
   };
   const addLose = () => {
-    setLoses((loses) => loses + 1);
+    if (loses >= 20) return;
+    setLoses((loses) => parseInt(loses) + 1);
+    localStorage.setItem('loses', loses + 1);
   };
   const removeLose = (e) => {
     e.preventDefault();
-    setLoses((loses) => loses - 1);
+    if (loses <= 0) return;
+    setLoses((loses) => parseInt(loses) - 1);
+    localStorage.setItem('loses', loses - 1);
   };
   const reset = () => {
     setWins(0);
     setLoses(0);
+    localStorage.setItem('loses', 0);
+    localStorage.setItem('wins', 0);
   };
+
+  useEffect(() => {
+    window.addEventListener('storage', () => {
+      window.location.reload();
+    });
+    return () => {
+      window.removeEventListener('storage', () => {
+        window.location.reload();
+      });
+    };
+  }, []);
 
   return (
     <div className='app'>
